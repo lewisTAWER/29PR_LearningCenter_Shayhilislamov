4ba819ed28085590322d772187677e38ebbb5100
lewisTAWER <r.shayhilislamov55@gmail.com>
2024-10-25 13:03:29 +0500

    Разработанные контексты данных.Шайхилисламов

---
+++ LearningCenter_Shayhilislamov/Context/CoursesContext.cs
@@ -0,0 +1,60 @@
+﻿using LearningCenter_Shayhilislamov.Classes.Common;
+using LearningCenter_Shayhilislamov.Classes;
+using LearningCenter_Shayhilislamov.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.ObjectModel;
+
+namespace LearningCenter_Shayhilislamov.Context
+{
+    public class CoursesContext : DbContext
+    {
+        public DbSet<Courses> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql(Config.ConnectionConfig, Config.Version);
+
+        public CoursesContext() => Database.EnsureCreated();
+
+        public static ObservableCollection<Courses> AllCourses()
+        {
+            using CoursesContext context = new();
+            return new ObservableCollection<Courses>(context.Courses);
+        }
+
+        public void Save(Courses coursesItem, bool isNew)
+        {
+            using CoursesContext context = new();
+            if (isNew)
+            {
+                context.Courses.Add(coursesItem);
+            }
+            else
+            {
+                context.Courses.Update(coursesItem);
+            }
+            context.SaveChanges();
+        }
+
+        public void Delete(Courses coursesItem)
+        {
+            using CoursesContext context = new();
+            context.Courses.Remove(coursesItem);
+            context.SaveChanges();
+        }
+
+        public RelayCommand OnSave
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    if (obj is Courses coursesItem)
+                    {
+                        Save(coursesItem, coursesItem.Id == 0);
+                        MainWindow.init?.frame.Navigate(new View.Courses.Main());
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ LearningCenter_Shayhilislamov/Context/StudentsContext.cs
@@ -0,0 +1,59 @@
+﻿using LearningCenter_Shayhilislamov.Classes.Common;
+using LearningCenter_Shayhilislamov.Classes;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.ObjectModel;
+using LearningCenter_Shayhilislamov.Model;
+
+namespace LearningCenter_Shayhilislamov.Context
+{
+    public class StudentsContext : DbContext
+    {
+        public DbSet<Students> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql(Config.ConnectionConfig, Config.Version);
+
+        public StudentsContext() => Database.EnsureCreated();
+        public static ObservableCollection<Students> AllStudents()
+        {
+            using StudentsContext context = new();
+            return new ObservableCollection<Students>([.. context.Students]);
+        }
+
+        public void Save(Students studentsItem, bool isNew)
+        {
+            using StudentsContext context = new();
+            if (isNew)
+            {
+                context.Students.Add(studentsItem);
+            }
+            else
+            {
+                studentsItem.Courses = null;
+                context.Students.Update(studentsItem);
+            }
+            context.SaveChanges();
+        }
+
+        public void Delete(Students studentsItem)
+        {
+            Students.Remove(studentsItem);
+            SaveChanges();
+        }
+
+        public RelayCommand OnSave
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    if (obj is Students studentsItem)
+                    {
+                        Save(studentsItem, studentsItem.Id == 0);
+                        MainWindow.init?.frame.Navigate(new View.Students.Main());
+                    }
+                });
+            }
+        }
+    }
+}
