d32aa55c8f3edc05026b54a851a9d6563134e61e
lewisTAWER <r.shayhilislamov55@gmail.com>
2024-10-25 12:43:28 +0500

    Подключение к БД.Шайхилисламов

---
+++ LearningCenter_Shayhilislamov/Classes/Common/Config.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LearningCenter_Shayhilislamov.Classes.Common
+{
+    public class Config
+    {
+        public static string ConnectionConfig = "server=127.0.0.1;port=3306;uid=root;pwd=;database=LearningCenter;SslMode=None;";
+
+        public static MySqlServerVersion Version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
