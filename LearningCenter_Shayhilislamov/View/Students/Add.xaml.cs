436a52227ff9cde2cb07c809a780dd6411f222bd
Рустам <r.shayhilislamov55@gmail.com>
2024-10-27 01:14:48 +0500

    Созданные страницы Students.Шайхилисламов

---
+++ LearningCenter_Shayhilislamov/View/Students/Add.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace LearningCenter_Shayhilislamov.View.Students
+{
+    /// <summary>
+    /// Логика взаимодействия для Add.xaml
+    /// </summary>
+    public partial class Add : Page
+    {
+        public Add(object? Context)
+        {
+            InitializeComponent();
+            DataContext = Context;
+        }
+    }
+}
