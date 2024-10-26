76c2203bb162a30b6f9a74ca2ebad6d480e3432d
Рустам <r.shayhilislamov55@gmail.com>
2024-10-27 00:29:28 +0500

    Созданные страницы Courses.Шайхилисламов

---
+++ LearningCenter_Shayhilislamov/View/Courses/Main.xaml.cs
@@ -0,0 +1,17 @@
+﻿using LearningCenter_Shayhilislamov.ViewModel;
+using System.Windows.Controls;
+
+namespace LearningCenter_Shayhilislamov.View.Courses
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main()
+        {
+            InitializeComponent();
+            DataContext = new VMCourses();
+        }
+    }
+}
